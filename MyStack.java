f08ae63a2c3f8256d8b92459bd279401eb86c4c1
Ranjithgc <ranjith.amca.16@acharya.ac.in>
2021-06-08 11:37:55 +0530

    Adding peak and other stack methods

--- MyStack.java
+++ MyStack.java
@@ -14,4 +14,29 @@ public class MyStack {
     public void printStack() {
         myLinkedList.PrintMyNodes();
     }
+    public INode peak() {
+        return myLinkedList.head;
+    }
+    public INode pop() {
+        return myLinkedList.pop();
+    }
+
+    public int size() {
+        return myLinkedList.size();
+    }
+
+    public void emptyingTheStack(){
+        while (size()!=0){
+            pop();
+        }
+    }
+
+    public boolean IsEmpty(){
+        if(size()==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
